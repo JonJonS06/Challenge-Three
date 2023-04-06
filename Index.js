3f771a464bdf62dcc773385a211a891692f4131a
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-04-06 13:57:20 -0400

    added variable declarations and a function generatePassword

--- Index.js
+++ Index.js
@@ -1,22 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// Will be using an array method!!
-// var finalPAssword = ('B', 'a');
+
 var number = ["0123456789"];
 var specialChar = ["!%&,*+-./<>?~"];
 var alphaLower = ["abcdefghijklmnopqrstuvwxyz"];
 var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumerals;
+var confirmUppercase;
+var confirmLowercase;
+
+
+function gerneratePassword() {
+    var confirmLength = (prompt("Select your password length between 8-128 characters."));
+
+    while(confirmLength <= 8 || confirmLength >= 128) {
+        alert("Password must be between 8-128 characters. Please try again.");
+        var confirmLength = (prompt("Select your password length between 8-128 characters."));
+}
+
+
+
+
+}
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
